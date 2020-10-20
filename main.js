a1ae82ebb57a3f936cfca374f773d533af88f979
Giulia <giulia.guerrazzi@live.it>
2020-10-20 19:22:42 +0200

    revision

--- main.js
+++ main.js
@@ -25,19 +25,3 @@ for (var i = 1; i <= numero; i++) {
   }
 
 }
-
-// var num = 100;
-//
-// for ( var i = 1; i <= num; i++ ) {
-//
-//     if ( i % 15 == 0  ) {
-//         console.log("FizzBuzz");
-//     } else if ( i % 3 == 0) {
-//         console.log("Fizz");
-//     } else if ( i % 5 == 0) {
-//         console.log("Buzz");
-//     } else {
-//         console.log(i + "");
-//     }
-//
-// }
